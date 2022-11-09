1bb81d783c7a9c29098d51f4acae08819b0616a0
Arfan beplob <98867719+Arfan-Beplob@users.noreply.github.com>
2022-11-09 23:13:42 +0600

    3rd Commit Some Change++

--- lib/main.dart
+++ lib/main.dart
@@ -54,119 +54,111 @@ class _ExpasionTileState extends State<ExpasionTile> {
                       Padding(padding: const EdgeInsets.all(8),
                         child: Column(
                           children: [
-                            Column(
-                              children: [
-                                InkWell(
-                                    onTap: (){
-                                      Navigator.push(context, MaterialPageRoute(
-                                          builder: (context)=>
-                                              FreeImage(Image:"https://c1.peakpx.com/wallpaper/703/608/953/lion-feline-big-cat-wallpaper-preview.jpg")));
-                                    },
-                                    child: const Card(child: ListTile(title: Text('Lion',
-                                      style: TextStyle(
-                                          fontWeight: FontWeight.bold,
-                                          fontSize: 20
+                            Card(child: ListTile(
+                                onTap: (){
+                                  Navigator.push(context, MaterialPageRoute(
+                                      builder: (context)=>
+                                          FreeImage(Image:"https://c1.peakpx.com/wallpaper/703/608/953/lion-feline-big-cat-wallpaper-preview.jpg")));
+                                },
+                                title: Text('Lion',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 20
+                              ),
+                            ))),
+                            Card(child: ListTile(
+                                onTap: (){
+                                  Navigator.push(context, MaterialPageRoute(
+                                      builder: (context)=>
+                                          FreeImage(Image:"https://cdn.stocksnap.io/img-thumbs/960w/tiger-animal_LK4FNPG2OA.jpg")));
+                                },
+                                title: Text('Tiger'))),
+                            Card(child: ListTile(
+                                onTap: (){
+                                  Navigator.push(context, MaterialPageRoute(
+                                      builder: (context)=>
+                                          FreeImage(Image:"https://i.pinimg.com/originals/23/98/4c/23984ccfc742d2b7af5d9639e4818eda.jpg")));
+                                },
+                                title: Text('Goat'))),
+                            Card(child: ListTile(
+                                onTap: (){
+                                  Navigator.push(context, MaterialPageRoute(
+                                      builder: (context)=>
+                                          FreeImage(Image:"https://i.pinimg.com/originals/28/26/d5/2826d58cfaca8cdadc134bd90e2a2415.jpg")));
+                                },
+                                title: Text('Horse'))),
+                             Card(child: ListTile(
+                                 onTap: (){
+                                   Navigator.push(context, MaterialPageRoute(
+                                       builder: (context)=>
+                                           FreeImage(Image:"https://storage.needpix.com/rsynced_images/donkey-4079716_1280.jpg")));
+                                 },
+                                 title: Text('Donkey.'))),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Card(
+                                color: Colors.yellow,
+                                child: ExpansionTile(
+                                  leading: const Icon(Icons.animation),
+                                  title: const Text('Animals',
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 40
+                                    ),),
+                                  subtitle: const Text('Free Animals Photos',
+                                    style: TextStyle(
+                                        fontSize: 20
+                                    ),),
+                                  children: <Widget>[
+                                    Padding(padding: const EdgeInsets.all(8),
+                                      child: Column(
+                                        children: [
+                                          Card(child: ListTile(
+                                              onTap: (){
+                                                Navigator.push(context, MaterialPageRoute(
+                                                    builder: (context)=>
+                                                        FreeImage(Image:"https://c1.peakpx.com/wallpaper/703/608/953/lion-feline-big-cat-wallpaper-preview.jpg")));
+                                              },
+                                              title: const Text('Lion',
+                                            style: TextStyle(
+                                                fontWeight: FontWeight.bold,
+                                                fontSize: 20
+                                            ),
+                                          ))),
+                                          Card(child:  ListTile(
+                                              onTap: (){
+                                                Navigator.push(context, MaterialPageRoute(
+                                                    builder: (context)=>
+                                                        FreeImage(Image:"https://cdn.stocksnap.io/img-thumbs/960w/tiger-animal_LK4FNPG2OA.jpg")));
+                                              },
+                                              title: const Text('Tiger'))),
+                                          Card(child:  ListTile(
+                                              onTap: (){
+                                                Navigator.push(context, MaterialPageRoute(
+                                                    builder: (context)=>
+                                                        FreeImage(Image:"https://i.pinimg.com/originals/23/98/4c/23984ccfc742d2b7af5d9639e4818eda.jpg")));
+                                              },
+                                              title: const Text('Goat'))),
+                                          Card(child:  ListTile(
+                                              onTap: (){
+                                                Navigator.push(context, MaterialPageRoute(
+                                                    builder: (context)=>
+                                                        FreeImage(Image:"https://i.pinimg.com/originals/28/26/d5/2826d58cfaca8cdadc134bd90e2a2415.jpg")));
+                                              },
+                                              title: const Text('Horse'))),
+                                          Card(child:  ListTile(
+                                              onTap: (){
+                                                Navigator.push(context, MaterialPageRoute(
+                                                    builder: (context)=>
+                                                        FreeImage(Image:"https://storage.needpix.com/rsynced_images/donkey-4079716_1280.jpg")));
+                                              },
+                                              title: const Text('Donkey.'))),
+                                        ],
                                       ),
-                                    )))),
-                                InkWell(
-                                    onTap: (){
-                                      Navigator.push(context, MaterialPageRoute(
-                                          builder: (context)=>
-                                              FreeImage(Image:"https://cdn.stocksnap.io/img-thumbs/960w/tiger-animal_LK4FNPG2OA.jpg")));
-                                    },
-                                    child: const Card(child: ListTile(title: Text('Tiger')))),
-                                InkWell(
-                                    onTap: (){
-                                      Navigator.push(context, MaterialPageRoute(
-                                          builder: (context)=>
-                                              FreeImage(Image:"https://i.pinimg.com/originals/23/98/4c/23984ccfc742d2b7af5d9639e4818eda.jpg")));
-                                    },
-                                    child: const Card(child: ListTile(title: Text('Goat')))),
-                                InkWell(
-                                    onTap: (){
-                                      Navigator.push(context, MaterialPageRoute(
-                                          builder: (context)=>
-                                              FreeImage(Image:"https://i.pinimg.com/originals/28/26/d5/2826d58cfaca8cdadc134bd90e2a2415.jpg")));
-                                    },
-                                    child: const Card(child: ListTile(title: Text('Horse')))),
-                                InkWell(
-                                    onTap: (){
-                                      Navigator.push(context, MaterialPageRoute(
-                                          builder: (context)=>
-                                              FreeImage(Image:"https://storage.needpix.com/rsynced_images/donkey-4079716_1280.jpg")));
-                                    },
-                                    child: const Card(child: ListTile(title: Text('Donkey.')))),
-                                Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Card(
-                                    color: Colors.yellow,
-                                    child: ExpansionTile(
-                                      leading: const Icon(Icons.animation),
-                                      title: const Text('Animals',
-                                        style: TextStyle(
-                                            fontWeight: FontWeight.bold,
-                                            fontSize: 40
-                                        ),),
-                                      subtitle: const Text('Free Animals Photos',
-                                        style: TextStyle(
-                                            fontSize: 20
-                                        ),),
-                                      children: <Widget>[
-                                        Padding(padding: const EdgeInsets.all(8),
-                                          child: Column(
-                                            children: [
-                                              Column(
-                                                children: [
-                                                  Card(child: ListTile(
-                                                      onTap: (){
-                                                        Navigator.push(context, MaterialPageRoute(
-                                                            builder: (context)=>
-                                                                FreeImage(Image:"https://c1.peakpx.com/wallpaper/703/608/953/lion-feline-big-cat-wallpaper-preview.jpg")));
-                                                      },
-                                                      title: const Text('Lion',
-                                                    style: TextStyle(
-                                                        fontWeight: FontWeight.bold,
-                                                        fontSize: 20
-                                                    ),
-                                                  ))),
-                                                  Card(child:  ListTile(
-                                                      onTap: (){
-                                                        Navigator.push(context, MaterialPageRoute(
-                                                            builder: (context)=>
-                                                                FreeImage(Image:"https://cdn.stocksnap.io/img-thumbs/960w/tiger-animal_LK4FNPG2OA.jpg")));
-                                                      },
-                                                      title: const Text('Tiger'))),
-                                                  Card(child:  ListTile(
-                                                      onTap: (){
-                                                        Navigator.push(context, MaterialPageRoute(
-                                                            builder: (context)=>
-                                                                FreeImage(Image:"https://i.pinimg.com/originals/23/98/4c/23984ccfc742d2b7af5d9639e4818eda.jpg")));
-                                                      },
-                                                      title: const Text('Goat'))),
-                                                  Card(child:  ListTile(
-                                                      onTap: (){
-                                                        Navigator.push(context, MaterialPageRoute(
-                                                            builder: (context)=>
-                                                                FreeImage(Image:"https://i.pinimg.com/originals/28/26/d5/2826d58cfaca8cdadc134bd90e2a2415.jpg")));
-                                                      },
-                                                      title: const Text('Horse'))),
-                                                  Card(child:  ListTile(
-                                                      onTap: (){
-                                                        Navigator.push(context, MaterialPageRoute(
-                                                            builder: (context)=>
-                                                                FreeImage(Image:"https://storage.needpix.com/rsynced_images/donkey-4079716_1280.jpg")));
-                                                      },
-                                                      title: const Text('Donkey.'))),
-                                                ],
-                                              )
-                                            ],
-                                          ),
-                                        )
-                                      ],
-                                    ),
-                                  ),
-                                )
-                              ],
+                                    )
+                                  ],
+                                ),
+                              ),
                             )
                           ],
                         ),
